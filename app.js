4e1c8c2fea2ff6be18f77484c377fc14ffa7f701
walishahllc <walishahllc@gmail.com>
2024-07-09 15:06:38 -0700

    References

---
+++ app.js
@@ -0,0 +1,22 @@
+// Array References:
+
+// References means address in memory its means if we commpare one to--- 
+// another one and if there value is also same but its always false 
+// because the array References is different (New array means new References)
+
+// let one = ['a'];
+// let two = ['a'];
+
+// one == two; (False)
+// one === two; (False)
+
+let arr = ['a', 'b', 'c'];
+let newarr = arr;
+
+// now
+
+// newarr == arr; (True);
+// newarr === arr; (True);
+
+// newarr == arr; (True);
+// because we can do arr equal to newarr then its References is now same thats why its shows True
